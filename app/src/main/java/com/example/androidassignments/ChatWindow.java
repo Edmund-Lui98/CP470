a86df9c23fb4d717ee5d4d5455f01acd31972d4a
EdmundLuii <edmund.lui1998@gmail.com>
2019-10-05 20:34:08 -0400

    just have #5 and what goes with it left

--- app/src/main/java/com/example/androidassignments/ChatWindow.java
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -3,12 +3,37 @@ package com.example.androidassignments;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ChatWindow extends AppCompatActivity {
+
+    ListView lst;
+    EditText msg;
+    Button send;
+
+    ArrayList<String> msgs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
+
+        lst = findViewById(R.id.lst);
+        msg = findViewById(R.id.msg);
+        send = findViewById(R.id.send);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                msgs.add(msg.getText().toString());
+            }
+        });
     }
+
 }
