fbf15d09efe9972c00fa310e14cc38694d7d4977
EdmundLuii <edmund.lui1998@gmail.com>
2019-11-03 16:32:40 -0500

    Finished Assignment 3, part 1

--- app/src/main/java/com/example/androidassignments/ChatDatabaseHelper.java
+++ app/src/main/java/com/example/androidassignments/ChatDatabaseHelper.java
@@ -3,34 +3,36 @@ package com.example.androidassignments;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class ChatDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "Messages.db";
-    public static final String TABLE_NAME = "Messages Table";
-    public static String KEY_ID;
-    public static String KEY_MESSAGE;
+    public static final String TABLE_NAME = "Messages_Table";
+    public static String KEY_ID = "_id";
+    public static String KEY_MESSAGE = "message";
     public static int VERSION_NUM = 1;
+    public static String ACTIVITY_NAME = "ChatDatabaseHelper";
 
     public ChatDatabaseHelper(Context ctx) {
         super(ctx, DATABASE_NAME, null, VERSION_NUM);
         SQLiteDatabase db = this.getWritableDatabase();
-
     }
-
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME + " ("
-                + KEY_ID + " INTEGER AUTOINCREMENT "
-                + KEY_MESSAGE + " TEXT not null);");
-
+        Log.i(ACTIVITY_NAME, "Calling onCreate");
+        db.execSQL("create table " + TABLE_NAME + "("
+                + KEY_ID + " integer primary key autoincrement, "
+                + KEY_MESSAGE + " text not null);");
 
     }
-
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.i(ChatDatabaseHelper.class.getName(),"Upgrading database from version" + oldVersion + " to " + newVersion);
         db.execSQL("drop table if exists " + TABLE_NAME);
+        VERSION_NUM = newVersion;
         onCreate(db);
     }
+
 }
--- app/src/main/java/com/example/androidassignments/ChatWindow.java
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -2,8 +2,12 @@ package com.example.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,15 +19,21 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import static com.example.androidassignments.ChatDatabaseHelper.DATABASE_NAME;
+import static com.example.androidassignments.ChatDatabaseHelper.KEY_MESSAGE;
+import static com.example.androidassignments.ChatDatabaseHelper.TABLE_NAME;
+
 public class ChatWindow extends AppCompatActivity {
 
     ListView lst;
     EditText msg;
     Button send;
 
-    ChatDatabaseHelper myDb;
+    ChatDatabaseHelper db;
 
-    ArrayList<String> msgs = new ArrayList<String>();
+    ArrayList<String> msgs = new ArrayList<>();
+
+    final static String ACTIVITY_NAME = "ChatWindow";
 
 
     @Override
@@ -35,7 +45,8 @@ public class ChatWindow extends AppCompatActivity {
         msg = findViewById(R.id.msg);
         send = findViewById(R.id.send);
 
-        myDb = new ChatDatabaseHelper(this);
+        db = new ChatDatabaseHelper(this);
+        final SQLiteDatabase database = db.getWritableDatabase();
 
         final ChatAdapter messageAdapter =new ChatAdapter( this );
         lst.setAdapter (messageAdapter);
@@ -45,9 +56,23 @@ public class ChatWindow extends AppCompatActivity {
             public void onClick(View v) {
                 msgs.add(msg.getText().toString());
                 messageAdapter.notifyDataSetChanged(); //this restarts the process of getCount()/getView()
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(KEY_MESSAGE, msg.getText().toString());
+                database.insert(TABLE_NAME,null, contentValues);
                 msg.setText("");
             }
         });
+
+        String GET_MESSAGES = "SELECT message FROM Messages_Table";
+        final Cursor cursor = database.rawQuery(GET_MESSAGES,null);
+        cursor.moveToFirst();
+        Log.i(ACTIVITY_NAME, "Cursor’s  column count =" + cursor.getColumnCount() );
+
+        while(cursor.moveToNext() ) {
+            String x = cursor.getString(0);
+            msgs.add(x);
+            Log.i(ACTIVITY_NAME, "SQL MESSAGE: " + x);
+        }
 
     }
     private class ChatAdapter extends ArrayAdapter<String> {
@@ -78,5 +103,12 @@ public class ChatWindow extends AppCompatActivity {
             return result;
 
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
+
     }
 }
