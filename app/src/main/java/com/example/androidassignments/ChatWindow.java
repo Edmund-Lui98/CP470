30fea440b2d97d72eeb7b229eb84bf42ef8171cd
EdmundLuii <edmund.lui1998@gmail.com>
2019-11-17 15:21:40 -0500

    I think I am done with this

--- app/src/main/java/com/example/androidassignments/ChatWindow.java
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -11,13 +11,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -30,14 +30,13 @@ public class ChatWindow extends AppCompatActivity {
     EditText msg;
     Button send;
 
-    ChatDatabaseHelper db;
+    static ChatDatabaseHelper db;
 
     public static ArrayList<String> msgs = new ArrayList<>();
 
     final static String ACTIVITY_NAME = "ChatWindow";
 
     FrameLayout x;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,29 +64,32 @@ public class ChatWindow extends AppCompatActivity {
                 msg.setText("");
             }
         });
-
-        String GET_MESSAGES = "SELECT message FROM Messages_Table";
+        String GET_MESSAGES = "SELECT message, id FROM Messages_Table";
         final Cursor cursor = database.rawQuery(GET_MESSAGES,null);
+
         cursor.moveToFirst();
         Log.i(ACTIVITY_NAME, "Cursor’s  column count =" + cursor.getColumnCount() );
 
-        while(cursor.moveToNext() ) {
-            String x = cursor.getString(0);
-            msgs.add(x);
-            Log.i(ACTIVITY_NAME, "SQL MESSAGE: " + x);
-        }
 
-        x = findViewById(R.id.frame);
-        String s;
-        if (x == null) {
-            s = "It didnt load";
-        } else {
-            s = "It loaded ";
+        if (msgs.isEmpty()) {
+            String a = cursor.getString(0);
+            msgs.add(a);
+
+            while (cursor.moveToNext()) {
+                a = cursor.getString(0);
+                msgs.add(a);
+                Log.i(ACTIVITY_NAME, "SQL MESSAGE: " + x);
+            }
         }
-        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
 
     }
     private class ChatAdapter extends ArrayAdapter<String> {
+
+        ChatDatabaseHelper db = new ChatDatabaseHelper(ChatWindow.this);
+        final SQLiteDatabase database = db.getWritableDatabase();
+        String GET_MESSAGES = "SELECT message, id FROM Messages_Table";
+        final Cursor cursor = database.rawQuery(GET_MESSAGES,null);
+
         public ChatAdapter(Context ctx) {
             super(ctx, 0);
         }
@@ -99,8 +101,11 @@ public class ChatWindow extends AppCompatActivity {
         public String getItem(int position) {
             return msgs.get(position);
         }
+
         public long getItemId(int position) {
-            return long;
+            cursor.moveToPosition(position);
+            Long x = Long.parseLong(cursor.getString(1));
+            return x;
         }
 
         public View getView(int position, View convertView, ViewGroup parent) {
@@ -116,9 +121,16 @@ public class ChatWindow extends AppCompatActivity {
             TextView message = result.findViewById(R.id.msg);
             message.setText(getItem(position)); // get the string at position
             return result;
-
         }
+        /*
+        private boolean deleteMsg(long id) {
+            return database.delete("Messages_Table", "id = ?", id);
+        }
+
+         */
     }
+
+
 
     @Override
     protected void onDestroy() {
